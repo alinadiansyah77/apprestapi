8a1d9af1cf18cab8d8524a0cf913e75733bba156
unknown <ali_nadiansyah@yahoo.com>
2021-03-07 01:06:08 +0700

    Update Controller.js

--- koneksi.js
+++ koneksi.js
@@ -4,7 +4,7 @@ const conn= mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'formed',
-    database: dbrestapi
+    database: 'dbrestapi'
 });
 
 conn.connect((err)=>{
